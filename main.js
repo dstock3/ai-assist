2adb4755cd2c130b4a8223c0a3da7291f2fc1be6
dstock3 <dstock3@gmail.com>
2023-03-06 11:47:40 -0600

    Added if else statement in main route to ensure data is delivered in expected format.

--- main.js
+++ main.js
@@ -7,13 +7,26 @@ const port = process.env.PORT || 3000;
 app.get('/general-system-info', async (req, res) => {
   try {
     const result = await getGeneralInfo();
-    res.send(result.data.choices[0].message.content);
+    if (result.data.choices[0].message.content) {
+      res.send(result.data.choices[0].message.content);
+    } else {
+      throw new Error('Unexpected API response')
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while retrieving system information');
   }
 });
 
+/*
+app.get('/get-memory-info', async (req, res) => {
+  try {
+
+  } catch {
+
+  }
+})
+*/
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
