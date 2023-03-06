5e51a9fef055d7fbba7f9bc5921258654d49dc85
dstock3 <dstock3@gmail.com>
2023-03-06 01:08:56 -0600

    experimenting with various queries based on system data.

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ require('dotenv').config();
 
 const run = async () => {
   const openai = new OpenAIApi(new Configuration({ apiKey: process.env.API_KEY }));
-  const systemData = await getSystemData();
+  //const systemData = await getSystemData();
+  const { cpu, mem, currentLoad } = await getSystemData();
+
   const messages = [
     {
       role: 'system',
-      content: `You are analyzing a personal computer and looking for ways to optimize it.`
+      content: `You are analyzing a personal computer, collecting data, and looking for ways to optimize it.`
     },
-    { role: 'user', content: `Current CPU usage is ${systemData.cpuUsage}%, current memory usage is ${(systemData.memUsage / 1024 / 1024 / 1024).toFixed(2)}GB, and the top five applications in terms of computational demand are: ${systemData.topProcesses.map(process => `${process.name} (${process.cpuUsage}% CPU usage)`).join(', ')}.` },
+    {
+        role: 'user',
+        content: `My CPU data is as follows: ${JSON.stringify(cpu)}, and the current load is ${JSON.stringify(currentLoad)}. My RAM data is as follows: ${JSON.stringify(mem)}. Summarize this data in plain English based solely on the information provided. As of right now, what is the status of the system in terms of performance?`
+    }
+    /*
+    { role: 'user', content: `Current CPU usage is ${systemData.cpuUsage}%, current memory usage is ${(systemData.memUsage / 1024 / 1024 / 1024).toFixed(2)}GB, and the top five applications in terms of computational demand are: ${systemData.topProcesses.map(process => `${process.name} (${process.cpuUsage}% CPU usage)`).join(', ')}.` },*/
   ];
 
   const completion = await openai.createChatCompletion({ model: 'gpt-3.5-turbo', messages });
--- modules/systemInfo.js
+++ modules/systemInfo.js
@@ -1,27 +1,38 @@
 const system = require('systeminformation');
 
 const getSystemData = async () => {
-    const cpuUsage = await system.currentLoad({ interval: 1000 }).catch((err) => {
-        console.error(err);
-    });
-      
-    const memUsage = await system.mem();
-    const processes = await system.processes({ path: 'name', sort: 'pcpu', reverse: true });
+  const cpu = await system.cpu();
+  const mem = await system.mem();
+  const cpuUsage = await system.currentLoad();
+  console.log(cpuUsage)
 
-    const topProcesses = processes.slice(0, 5)
-        .map((process) => ({ name: process.name, cpuUsage: process.pcpu }));
+  /*
+  const cpuUsage = await system.currentLoad({ interval: 1000 });
+  
+  const processes = await system.processes({ path: 'name', sort: 'pcpu', reverse: true });
 
-    console.log({
-        cpuUsage: cpuUsage.currentload.toFixed(2),
-        memUsage: memUsage.used,
-        topProcesses,
-    });
+  const topProcesses = processes.slice(0, 5)
+    .map((process) => ({ name: process.name, cpuUsage: process.pcpu }));
 
-    return {
-        cpuUsage: cpuUsage.currentload.toFixed(2),
-        memUsage: memUsage.used,
-        topProcesses,
-    };
+  console.log({
+    cpuName: cpu.brand,
+    cpuCores: cpu.cores,
+    cpuSpeed: cpu.speed,
+    cpuUsage: cpuUsage.currentLoad.toFixed(2),
+    memUsage: memUsage.used,
+    topProcesses,
+  });
+
+  return {
+    cpuName: cpu.brand,
+    cpuCores: cpu.cores,
+    cpuSpeed: cpu.speed,
+    cpuUsage: cpuUsage.currentLoad.toFixed(2),
+    memUsage: memUsage.used,
+    topProcesses,
+  };
+  */
+ return { cpu, mem, cpuUsage }
 };
 
 module.exports = { getSystemData };
